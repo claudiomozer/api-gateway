b0dda672796c5178c813839b84fecb77adcaf72b
= <=>
2021-10-21 20:50:01 -0300

    Estrutura do Nest para iniciar a migração dos módulos de jogadores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CategoriasModule } from './categorias/categorias.module';
+import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [ CategoriasModule ],
+  imports: [ CategoriasModule, JogadoresModule ],
   controllers: [],
   providers: [],
 })
