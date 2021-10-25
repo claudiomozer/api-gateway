69c26e1fde8b7e29db17b83d9c1f64b4bded4616
= <=>
2021-10-24 21:23:28 -0300

    Implementa a estrutura inicial do módulo de desafios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoriasModule } from './categorias/categorias.module';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
-  imports: [ CategoriasModule, JogadoresModule ],
+  imports: [ CategoriasModule, JogadoresModule, DesafiosModule ],
   controllers: [],
   providers: [],
 })
