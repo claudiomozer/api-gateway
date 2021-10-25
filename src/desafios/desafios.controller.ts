85f342782d04b9054c67d070fddbfc184c1b2dc9
= <=>
2021-10-25 20:40:12 -0300

    Adiciona novo cliente para a fila de desafios

--- src/desafios/desafios.controller.ts
+++ src/desafios/desafios.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { ClientAdminBackendService } from 'src/infrastructure/services/client-admin-backend.service';
+import { ClientDesafiosService } from 'src/infrastructure/services/client-desafios.service';
 
 @Controller('api/v1/desafios')
-export class DesafiosController {}
+export class DesafiosController 
+{
+    private readonly clientAdminBackendService: ClientAdminBackendService;
+    private readonly clientDesafiosService: ClientDesafiosService;
+
+    constructor (
+        clientAdminBackendService: ClientAdminBackendService,
+        clientDesafiosService: ClientDesafiosService
+    ) {
+        this.clientAdminBackendService = clientAdminBackendService;
+        this.clientDesafiosService = clientDesafiosService;
+    }
+}
--- src/desafios/desafios.module.ts
+++ src/desafios/desafios.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ClientAdminBackendService } from 'src/infrastructure/services/client-admin-backend.service';
+import { ClientDesafiosService } from 'src/infrastructure/services/client-desafios.service';
 import { DesafiosController } from './desafios.controller';
 
 @Module({
+  providers: [ ClientDesafiosService, ClientAdminBackendService ],
   controllers: [DesafiosController]
 })
 export class DesafiosModule {}
