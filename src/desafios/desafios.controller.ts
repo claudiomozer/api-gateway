49de1224bd2f420634cf9313cb40717ef574094f
= <=>
2021-10-25 21:46:26 -0300

    Implementa o método de criar desafios

--- src/desafios/desafios.controller.ts
+++ src/desafios/desafios.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 import { ClientAdminBackendService } from 'src/infrastructure/services/client-admin-backend.service';
 import { ClientDesafiosService } from 'src/infrastructure/services/client-desafios.service';
+import { CriarDesafioDto } from './dtos/criar-desafio-dto';
 
 @Controller('api/v1/desafios')
 export class DesafiosController 
@@ -14,5 +16,48 @@ export class DesafiosController
     ) {
         this.clientAdminBackendService = clientAdminBackendService;
         this.clientDesafiosService = clientDesafiosService;
+    }
+
+    @Post('')
+    async criarDesafio (
+        @Body() criarDesafioDto: CriarDesafioDto
+    ) {
+        const { categoria } = criarDesafioDto;
+        const { jogadores } = criarDesafioDto;
+        const { solicitante } = criarDesafioDto;
+
+        const jogadorSolicitante = jogadores.filter(jogador => {
+            return jogador._id === solicitante;
+        })
+        
+        if (!jogadorSolicitante.length) {
+            throw new BadRequestException("O Jogador solicitante deve ser um dos jogadores da partida");
+        }
+
+        const jogadoresCategoria = jogadores.filter( jogador => {
+            return jogador.categoria === categoria;
+        });
+
+        if (!jogadoresCategoria || (jogadoresCategoria && (jogadoresCategoria.length < 2))) {
+            throw new BadRequestException("Todos os jogadores devem pertencer à categoria informada");
+        }
+
+        let categoriaEncontrada = this.clientAdminBackendService.client().send('consultar-categorias', categoria);
+        categoriaEncontrada = await lastValueFrom(categoriaEncontrada);
+        if ('error' in categoriaEncontrada) {
+            throw new BadRequestException('A categoria informada não existe');
+        }
+
+        const jogadoresIds = jogadores.map(jogador => jogador._id);
+        
+        let jogadoresEncontradosObservable = this.clientAdminBackendService.client().send('consultar-jogadores-ids', jogadoresIds);
+        const jogadoresEncontrados = await lastValueFrom(jogadoresEncontradosObservable);
+        if ('error' in jogadoresEncontrados) {
+            throw new BadRequestException('Nenhum dos jogadores informados existe');
+        } else if (jogadoresEncontrados.length !== 2) {
+            throw new BadRequestException('Um dos jogadores informados não foi cadastrado');
+        }
+
+        this.clientDesafiosService.client().emit('criar-desafio', { desafio: criarDesafioDto });
     }
 }
--- src/desafios/desafios.module.ts
+++ src/desafios/desafios.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ClientAdminBackendService } from 'src/infrastructure/services/client-admin-backend.service';
 import { ClientDesafiosService } from 'src/infrastructure/services/client-desafios.service';
 import { DesafiosController } from './desafios.controller';
 
 @Module({
-  providers: [ ClientDesafiosService, ClientAdminBackendService ],
+  providers: [ ConfigService, ClientDesafiosService, ClientAdminBackendService ],
   controllers: [DesafiosController]
 })
 export class DesafiosModule {}
