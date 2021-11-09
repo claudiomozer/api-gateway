2b25e53a3f0cea03e2149371067c82d195e3d39a
= <=>
2021-11-08 22:15:03 -0300

    Ajustes para a inserção dos rankings

--- src/desafios/dtos/atribuir-partida-desafio.dto.ts
+++ src/desafios/dtos/atribuir-partida-desafio.dto.ts
@@ -1,11 +1,10 @@
 import { IsNotEmpty } from "class-validator";
-import { Jogador } from "src/jogadores/interfaces/jogador.interface";
 import { Resultado } from "../interfaces/partida.interface";
 
 export class AtribuirPartidaDesafioDto
 {
     @IsNotEmpty()
-    def: Jogador;
+    def: string;
     
     @IsNotEmpty()
     resultado: Array<Resultado>;
